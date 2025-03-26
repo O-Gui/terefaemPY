d4fc75976f4f72002871d02a9168b779c6115085
Guilherme Basilio <137916326+O-Gui@users.noreply.github.com>
2025-03-26 10:28:58 -0300

    pedra-papel-tesoura.py

--- pedra-papel-tesoura.py
+++ pedra-papel-tesoura.py
@@ -1,4 +1,6 @@
 import random
+import time
+
 print("")
 print("O JOGO")
 print("")
@@ -15,23 +17,36 @@ print("")
 
 entrada = input("Digite sua escolha: ").strip().lower()
 
+def jokenpo ():
+    time.sleep(1)
+    print("JO")
+    time.sleep(1)
+    print("KEN")
+    time.sleep(1)
+    print("PO")
+    
+
+jokenpo()
+
 print("")
 
 escolhas = ["pedra", "papel", "tesoura"]
 
 if entrada not in escolhas:
-    print("Escolha inválida! Tente novamente.")
+   print("Escolha inválida! Tente novamente.")
 else:
-    
-    computador = random.choice(escolhas)
-    print(f"O computador escolheu: {computador}")
+   computador = random.choice(escolhas)
+   print(f"O computador escolheu: {computador}")
 
-    
-    if entrada == computador:
-        print("Empate!")
-    elif (entrada == "pedra" and computador == "tesoura") or \
-         (entrada == "papel" and computador == "pedra") or \
-         (entrada == "tesoura" and computador == "papel"):
-        print("Você venceu!")
-    else:
-        print("Você perdeu!")
+
+if entrada == computador:
+    print("Empate!")
+    print("")
+elif (entrada == "pedra" and computador == "tesoura") or \
+        (entrada == "papel" and computador == "pedra") or \
+        (entrada == "tesoura" and computador == "papel"):
+    print("Você venceu!")
+    print("")
+else:
+    print("Você perdeu!")
+    print("")
